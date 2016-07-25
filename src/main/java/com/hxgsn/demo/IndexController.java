64ed00ee181ee8415776dc8446e926e98d5169fb
Guocheng Zheng <121431418@qq.com>
2016-07-25 21:54:35 +0800

    JFinal控制器详解_getPara接收数据

--- src/main/java/com/hxgsn/demo/IndexController.java
+++ src/main/java/com/hxgsn/demo/IndexController.java
@@ -17,29 +17,31 @@ public class IndexController extends Controller {
 //    }
 
     public void index() {
-        String value1 = getPara("key1");
-
-        System.out.println("value1=====" + value1);
-        System.out.println("value2=====" + getPara("key2"));
-
-        String para = getPara();
-
-        System.out.println("para:" + para);
-        System.out.println("para0:" + getPara(0));
-        System.out.println("para1:" + getPara(1, "test"));
+//        String value1 = getPara("key1");
+//
+//        System.out.println("value1=====" + value1);
+//        System.out.println("value2=====" + getPara("key2"));
+//
+//        String para = getPara();
+//
+//        System.out.println("para:" + para);
+//        System.out.println("para0:" + getPara(0));
+//        System.out.println("para1:" + getPara(1, "test"));
 //        System.out.println("para2:" + URLDecoder.decode(getPara(2, "")));
+//
+//        int para2 = getParaToInt(2, 100);
+//
+//        System.out.println("para2:" + para2);
 
-        int para2 = getParaToInt(2, 100);
-
-        System.out.println("para2:" + para2);
-
-        String title = getPara("title", "这是设置默认标题");
+        String title = getPara("title", "这是设置的默认标题");
         String subtitle = getPara("subtitle");
         String content = getPara("content");
 
         System.out.println("title---->" + title);
         System.out.println("subtitle---->" + subtitle);
         System.out.println("content---->" + content);
+
+
 
         render("/index.html");
     }
