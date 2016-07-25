599d9e15ee57dc861b2512dcf07ece930786d9e7
Guocheng Zheng <121431418@qq.com>
2016-07-25 22:24:40 +0800

    JFinal控制器详解_getModel和getBean批量数据接收

---
+++ src/main/java/com/hxgsn/demo/Article.java
@@ -0,0 +1,54 @@
+package com.hxgsn.demo;
+
+/**
+ * Created by zgc on 16-7-25.
+ */
+public class Article {
+
+    private long id;
+    private String title;
+    private String subtitle;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", subtitle='" + subtitle + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
